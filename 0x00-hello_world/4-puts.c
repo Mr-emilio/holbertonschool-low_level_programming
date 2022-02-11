0f06e9478cfb12c8382c8356fca92af9cf06c776
emilio <pablopeag@hotmail.com>
2022-02-10 22:00:54 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-/*if dont read documentation, pls dont copy
- my first cprogram
-*/
+/*
+ * if dont read documentation, pls dont copy
+ * my first cprogram
+ */
+/**
+ * main - entry point, dont take arguments. print the sentence
+ * Return: return always 0
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
