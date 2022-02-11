8e9fe2ae6dbfd8cdae530f7429dc280c55a372fb
emilio <pablopeag@hotmail.com>
2022-02-10 21:22:08 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*if dont read documentation, pls dont copy
+ my first cprogram
+*/
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
