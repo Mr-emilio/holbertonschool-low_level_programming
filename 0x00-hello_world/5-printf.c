62fd60f1d7639eedf1a98663c3034ec1898d1419
emilio <pablopeag@hotmail.com>
2022-02-14 18:10:56 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *if dont read the documentation, pls dont copy
+ *my first cprogram -m6402
+ */
+/**
+ * main - entry point and dont take arguments. print the sentence
+ * Return: retur 0 by requirements
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
