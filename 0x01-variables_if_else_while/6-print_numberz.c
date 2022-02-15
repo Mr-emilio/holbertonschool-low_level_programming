3e45b9f570ab27ff3341e406ba7ccb281bea5cff
emilio <pablopeag@hotmail.com>
2022-02-14 20:24:02 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*read the documentation about putchar -Mr 4602 and for loops*/
+/**
+ * main - print the numbers with putchar
+ * Return: return 0 for conditions
+ */
+int main(void)
+{
+int n;
+for (n = "0"; n < "10"; n++)
+	putchar(n);
+putchar("\n");
+return (0);
+}
