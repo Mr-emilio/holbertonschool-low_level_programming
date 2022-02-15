0984bdec7d62aad0c0a612ce778dc660ede9688b
emilio <pablopeag@hotmail.com>
2022-02-14 20:12:39 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*read the documentation about for loops -mr 4602*/
+/**
+ * main - print the numbers
+ * Return: return 0 for conditions
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+	printf("%d", n);
+printf("\n");
+return (0);
+}
