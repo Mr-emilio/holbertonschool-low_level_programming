bccee194d82e769c389f4067062ce24daace5919
emilio <pablopeag@hotmail.com>
2022-02-14 18:16:39 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-printf("size of a char: %zu bype(s)\n", sizeof(char));
-printf("size of a int: %zu bypes(s)\n", sizeof(int));
-printf("size of a long int: %zu bypes(s)\n", sizeof(long int));
-printf("size of a long long int: %zu bype(s)\n", sizeof(long long int));
-printf("size of a float: %zu bype(s)\n", sizeof(float));
+printf("size of a char: %zu byte(s)\n", sizeof(char));
+printf("size of a int: %zu byte(s)\n", sizeof(int));
+printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("size of a float: %zu byte(s)\n", sizeof(float));
 }
