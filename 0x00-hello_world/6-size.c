3a7fde7467e4818ad4373b8f379c19517759d96c
emilio <pablopeag@hotmail.com>
2022-02-14 18:13:35 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * if dont read the documentation dont copy maby wron
+ */
+/**
+ * main - print the sentences with the size of various types
+ * Return: return 0 by requirements
+ */
+int main(void)
+{
+printf("size of a char: %zu bype(s)\n", sizeof(char));
+printf("size of a int: %zu bypes(s)\n", sizeof(int));
+printf("size of a long int: %zu bypes(s)\n", sizeof(long int));
+printf("size of a long long int: %zu bype(s)\n", sizeof(long long int));
+printf("size of a float: %zu bype(s)\n", sizeof(float));
+}
