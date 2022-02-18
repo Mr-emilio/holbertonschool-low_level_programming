f9385383f8538a56bb28dd3099c477b16bd66f5a
emilio <pablopeag@hotmail.com>
2022-02-18 11:27:03 -0800

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,12 @@
  */
 void print_most_numbers(void)
 {
-	int n;
-	for (n = '0'; n <= '9'; n++)
-	{
-		if (n != '2' && n != '4')
-		_putchar(x);
-		_putchar('\n');
-	}
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+	if (n != '2' && n != '4')
+	_putchar(n);
+	_putchar('\n');
+}
 }
 
