ea4dbadfabfeccbb73894453934f0532c8f78a29
emilio <pablopeag@hotmail.com>
2022-02-18 10:06:12 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,12 @@
  */
 void print_numbers(void)
 {
-	int n;
-	for (n = '0'; n <= '9'; n ++)
+int n;
+
+for (n = '0'; n <= '9'; n++)
 	{
-		_putchar(n);
-		_putchar('\n');
+	_putchar(n);
+	_putchar('\n');
 	}
+
 }
