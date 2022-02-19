0a155e78e2e9db80a91f3fb08d7ea4eac5b261fa
emilio <pablopeag@hotmail.com>
2022-02-18 19:44:01 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - my funtion that print a line
+ * @n: is the number of times the _ should be printed
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	putchar('\n');
+}
