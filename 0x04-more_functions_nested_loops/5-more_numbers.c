8a5eeddd7bdb6fef16c8cba50710c5e0cfd6c216
emilio <pablopeag@hotmail.com>
2022-02-18 18:48:47 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - my main funtion print 1 through 14, 10 times
+ */
+void more_numbers(void)
+{
+	int veces;
+	char nu;
+	
+	for (veces = 0; veces < 10; veces++)
+	{
+	for (nu = 0; nu <= 14; nu++)
+	{
+		if (p > 9)
+		_putchar((p / 10) + '0');
+		_putchar((p % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
+
