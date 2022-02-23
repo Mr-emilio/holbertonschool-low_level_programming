343ae670f21536adcdb4371b6c7d7cd2a977a424
emilio <pablopeag@hotmail.com>
2022-02-22 20:39:16 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - function main
+ * @str: pointer type char
+ */
+void puts2(char *str)
+{
+int i = 0; j = 0; k;
+char *p = str;
+
+while (*p != '\0')
+{
+	p++;
+	i++;
+}
+j = i - 1;
+for (k = 0; k <= j; k++)
+{
+	if (k % 2 == 0)
+		_putchar(str[k]);
+}
+_putchar('\n');
+}
