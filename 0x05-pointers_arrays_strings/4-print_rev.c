0e63685f35918ce28079c99151c2ebabef6e8908
emilio <pablopeag@hotmail.com>
2022-02-22 20:14:01 -0800

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - function main, print str in reverse
+ * @s: the str
+ */
+void print_rev(char *s)
+{
+int i = 0, j;
+
+while (*s != '\0')
+{
+	i++;
+	j++;
+}
+s--;
+for (j = i; j > 0; j--)
+{
+	_putchar(*s);
+	s--;
+}
+_putchar('\n');
+}
