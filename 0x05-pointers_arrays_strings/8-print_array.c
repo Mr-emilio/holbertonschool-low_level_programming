3232269a7e86d15fc87dee67d2ff89a32d3c5b79
emilio <pablopeag@hotmail.com>
2022-02-22 20:56:45 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - function main
+ * @a: pointer type int
+ * @n: variable type int
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i< n; i++)
+{
+	printf("%d", a[i]);
+	if (i != n -1)
+	{
+		printf(", ");
+	}
+}
+printf("\n");
+}
