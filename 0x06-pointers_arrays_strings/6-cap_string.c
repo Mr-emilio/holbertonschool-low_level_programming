89a5bb304fda613089329e56a8dff686c29a2d33
emilio <pablopeag@hotmail.com>
2022-02-24 20:41:48 -0800

    capitalitation string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *cap_string - function main
+ * @p: pointer type char
+ * Return: capitalized words in p
+ */
+char *cap_string(char *p)
+{
+	int i, j;
+	char *sig = ",;.!?\"(){} \n\t";
+
+	for (i = 0; *(p + i) != '\0'; i++)
+	{
+		if (*(p + i) >= 'a' && *(p + i) <= 'z')
+			for (j = 0; *(sig + j) != '\0'; j++)
+			{
+				if (*(p + i -1) == *(sig + j))
+				{
+					*(p + i) -= 32;
+					break;
+				}
+				else if (i == 0)
+				{
+					*(p + i) -= 32;
+					break;
+				}
+			}
+	}
+	return (a);
+}
