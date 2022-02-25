645c6cc59a7672cab04601a491e432c0eac3533c
emilio <pablopeag@hotmail.com>
2022-02-24 19:27:07 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,8 +9,13 @@ char *_strcat(char *dest, char *src)
 int hello = 0, world = 0;
 
 while (dest[hello] != '\0')
+{
 	hello++;
+}
+
 while ((dest[hello + world] = *src++) != '\0')
+{
 	world++;
+}
 return (dest);
 }
