790a68660bd214d9d15c1ca22650796f71a3953f
emilio <pablopeag@hotmail.com>
2022-02-24 20:05:10 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - function main
+ * @s1: pointer type char
+ * @s2: pointer type char
+ * Return: return 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
