0380b42c5c4eb9b9537ca35911e976ce49952a99
emilio <pablopeag@hotmail.com>
2022-02-24 19:35:34 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncat - function main
+ * @dest: pointer type char
+ * @src: pointer type char
+ * @n: variable type int
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+while (dest[i] != '\0')
+{
+	i++;
+}
+while (j < n && src[j] != '\0')
+{
+	dest[i] = src[i];
+	i++;
+	j++;
+}
+dest[i] = '\0';
+return (dest);
+}
