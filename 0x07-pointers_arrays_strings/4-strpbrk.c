9982e09fd1cad5ebbb4c5e66faf73cf091490329
emilio <pablopeag@hotmail.com>
2022-02-28 20:45:41 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,13 +3,13 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: pointer type char
  * @accept: pointer type char
- * Return: NULL or the number of bytes
+ * Returni: NULL or the number of bytes
  */
 char *_strpbrk(char *s, char *accept)
 {
 unsigned int i, j;
 
-for (a = 0; *(s + i) != 00; i++)
+for (i = 0; *(s + i) != 00; i++)
 {
 	for (j = 0; *(accept + j) != 00; j++)
 	{
