8dd6cb660ce2e44310cb6d41f8d5c92a1ea8facc
emilio <pablopeag@hotmail.com>
2022-03-02 20:48:54 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	return (2sqrt(n, 0));
+	return (dos_sqrt(n, 0));
 }
 /**
  * sqrt - function for sqrt of number
@@ -17,7 +17,7 @@ int _sqrt_recursion(int n)
  * @i: variable type int
  * Return: variable i
  */
-int 2sqrt(int n, int i)
+int dos_sqrt(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
@@ -25,5 +25,5 @@ int 2sqrt(int n, int i)
 	if (i * i == n)
 		return (i);
 
-	return (2sqrt(n, i + 1));
+	return (dos_sqrt(n, i + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int 2sqrt(int n, int i);
+int dos_sqrt(int n, int i);
 
 
 #endif
