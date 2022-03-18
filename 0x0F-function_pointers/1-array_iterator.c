e8ff59faad066951d19295ac2ed719580902a655
emilio <pablopeag@hotmail.com>
2022-03-17 21:21:04 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pinters.h"
+/**
+ * array_iterator - function main 
+ * @array: array pointer to be processed
+ * @size: size of the array
+ * @action: function to used
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+	if (array && action)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
+}
