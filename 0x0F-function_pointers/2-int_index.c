cbb6c51cc64643cb840a12d27c6082beb7ecaedc
emilio <pablopeag@hotmail.com>
2022-03-17 21:50:05 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "finction_pointers.h"
+/**
+ * int_index - function main searches ints
+ * @array: array pointer 
+ * @size: size of array
+ * @cmp: pointer used to compare values
+ * Return: -1 if no matches elements or index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array && cmp)
+	{
+		for (; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
