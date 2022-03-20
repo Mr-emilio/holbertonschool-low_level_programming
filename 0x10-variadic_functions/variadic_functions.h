198c1664e20054bb25bb89f81269bcb78c109832
emilio <pablopeag@hotmail.com>
2022-03-19 21:42:00 -0700

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_C
-#define MAIN_C
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
