b6c975299948b5a1b7a050ba515b5d9f231d728b
Mr-emilio <pablopeag@hotmail.com>
2022-03-30 23:52:51 -0500

    last task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * premain - prints a message before the main function is executed
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
