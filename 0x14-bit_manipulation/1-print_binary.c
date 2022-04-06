2ec73539a72be9b92ac8387513eab7c964bea787
Mr-emilio <pablopeag@hotmail.com>
2022-04-05 23:06:25 -0500

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_binary - prints binary format of number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+
+		putchar((n & 1) + '0');
+	}
+	else
+		putchar('0');
+}
