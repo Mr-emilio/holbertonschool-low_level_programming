3dd283c96d04a6f79b787e4c0b8731661392cef0
Mr-emilio <pablopeag@hotmail.com>
2022-06-06 22:25:05 -0500

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_table.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp;
+
+	if (ht)
+	{
+		while (i < ht->size)
+		{
+			tmp = ht->array[i];
+			while (tmp)
+			{
+				ht->array[i] = ht->array[i]->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				tmp = ht->array[i];
+			}
+			i++;
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
